bb32e18ba623d8bc86d1ed431a0efc43d7bf6f6d
Pedro Alencar <pedromariutti@gmail.com>
2023-05-28 17:04:40 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//TODO Ex 1 - Crie um novo projeto no intelliJ para uma aplicação java.
+//TODO Ex 2 - Crie um Package (pasta / pacote) com nome entidades.
+//TODO Ex 3 - Crie classes com o nome Produto, ProdutoUsado e ProdutoImportado no pacote entidades.
+//TODO Ex 4 - Crie um novo arquivo (se não houver no projeto inicial) contendo um método main().
+//TODO Ex 5 - No método main() crie uma Lista para armazenar os produtos.
+//TODO Ex 6 - O Programa vai receber as informações referentes aos tipos de produto.
+//TODO Ex 7 - Percorrer a lista de produtos e chamar o método pra mostrar as informações dos produtos.
+//TODO Ex 8 - o programa deve ter saída desse modo.
+//        Entre com o número de produtos: 3
+//        dados Produto#1:
+//        Comum, usado ou importado(c/u/i)? i
+//        Nome: Tablet
+//        Preço: 260.00
+//        Taxa: 20.00
+//        dados Produto#2:
+//        Comum, usado ou importado(c/u/i)? c
+//        Nome: Notebook
+//        Preço: 1100.00
+//        dados Produto#3:
+//        Comum, usado ou importado(c/u/i)? u
+//        Nome: Iphone
+//        Preço: 400.00
+//        Data de fabricação (DD/MM/YYYY): 15/03/2017
+//        Amostragem de preço dos produtos:
+//        Tablet R$ 280.00 (Taxa: R$ 20.00)
+//        Notebook R$ 1100.00
+//        Iphone (usado) R$ 400.00 (Data de fabricação: 15/03/2017
